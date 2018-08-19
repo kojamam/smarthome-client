6bb4eb2f6953b680dbd934422b0522ceafa32026
Koji Yamamoto <kyamuise@gmail.com>
2018-08-19 13:32:36 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+var WebSocketClient = require('websocket').client;
+const exec = require('child_process').exec;
+const signals = require('./config/signals');
+const sendData = require('./lib/sendData');
+
+dotenv.config();
+var client = new WebSocketClient();
+const host = process.env.WS_HOST;
+const recconectInterval = parseInt(process.env.RECONNECT_INTERVAL);
+
+client.on('connectFailed', function (error) {
+    console.log('Connect Error: ' + error.toString());
+    setRecconectTimer();
+});
+
+client.on('connect', function (connection) {
+    console.log('WebSocket Client Connected');
+
+    connection.on('error', function (error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function () {
+        console.log('Connection Closed');
+        setRecconectTimer();
+    });
+    connection.on('message', function (message) {
+        console.log(message);
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+            let messageJson = JSON.parse(message.utf8Data);
+            //TODO error handling
+            console.log(messageJson.commands);
+            messageJson.commands.forEach(command => {
+                sendData(signals[command]);
+
+            });
+        }
+    });
+});
+
+const connect = () => {
+    client.connect(host, 'khp', process.env.ORIGIN);
+}
+
+const setRecconectTimer = () => {
+    setTimeout(() => {
+        connect();
+    }, recconectInterval);
+}
+
+connect();
