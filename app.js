5bc3742ef8f5312caedce4a4ced79ff49bb0f5d6
Koji Yamamoto <kyamuise@gmail.com>
2019-03-24 15:07:18 +0900

    support TPLink

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const dotenv = require('dotenv');
 var WebSocketClient = require('websocket').client;
 const exec = require('child_process').exec;
 const signals = require('./config/signals');
-const sendData = require('./lib/sendData');
+const sendToBloadlink = require('./lib/sendToBloadlink');
+const getTPLinkDevice = require('./lib/getTPLinkDevice');
 
 dotenv.config();
 var client = new WebSocketClient();
 const host = process.env.WS_HOST;
 const recconectInterval = parseInt(process.env.RECONNECT_INTERVAL);
+
 
 client.on('connectFailed', function (error) {
     console.log('Connect Error: ' + error.toString());
@@ -26,25 +28,34 @@ client.on('connect', function (connection) {
     });
     connection.on('message', function (message) {
         if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
+            console.log(new Date().toLocaleString() + " Received: '" + message.utf8Data + "'");
             let messageJson = JSON.parse(message.utf8Data);
             //TODO error handling
-            messageJson.commands.forEach(command => {
-                sendData(signals[command]);
-
+            messageJson.commands.forEach((command) => {
+                switch (command.deviceType) {
+                    case "bloadlink-rm":
+                        sendToBloadlink(signals[command.signalName]);
+                        break;
+                    case "tplink-plug":
+                        getTPLinkDevice(command.alias).setPowerState(command.state);
+                        break;
+                    default:
+                        console.log("unsupported command");
+                        break;
+                }
             });
         }
     });
 });
 
-const connect = () => {
+const connectServer = () => {
     client.connect(host, 'hcp', process.env.ACCESS_TOKEN); //hcp=home control protocol
 }
 
 const setRecconectTimer = () => {
     setTimeout(() => {
-        connect();
+        connectServer();
     }, recconectInterval);
 }
 
-connect();
+connectServer();
